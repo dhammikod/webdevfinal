bf0ce17059a1f5f44e80f059007902ee84b83243
dhammikod <63945345+dhammikod@users.noreply.github.com>
2022-12-02 14:07:43 +0700

    3js wuapik
    asdfasdf

--- public/js/main.js
+++ public/js/main.js
@@ -8,8 +8,8 @@ document.body.appendChild( renderer.domElement );
 renderer.shadowMap.enabled = true;
 
 //for helping the development process
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
 
 //orbit camera
 const orbit = new OrbitControls(camera, renderer.domElement);
@@ -18,18 +18,34 @@ camera.position.y = 3;
 //must be declared after camera position declaration
 orbit.update();
 
+//bloommm
+const renderScene = new RenderPass(scene, camera);
+const composer = new EffectComposer(renderer);
+
+composer.addPass(renderScene);
+
 //creating the plane
-const planeform = new THREE.PlaneGeometry(100,100);
-const planecolour = new THREE.MeshStandardMaterial({color: 0xFFFFFF});
-const plane = new THREE.Mesh(planeform, planecolour);
-scene.add(plane);
-plane.rotation.x = -0.5 * Math.PI;
-plane.receiveShadow = true;
+// const planeform = new THREE.PlaneGeometry(100,100);
+// const planecolour = new THREE.MeshStandardMaterial({color: 0xFFFFFF});
+// const plane = new THREE.Mesh(planeform, planecolour);
+// scene.add(plane);
+// plane.rotation.x = -0.5 * Math.PI;
+// plane.receiveShadow = true;
+
+
+//BLOOMMMM
+const bloomPass = new UnrealBloomPass(
+	new THREE.Vector2(window.innerWidth, window.innerHeight), 
+	1.6,
+	0.1,
+	0.1
+);
+composer.addPass(bloomPass);
 
 //membuat object
 const loader = new GLTFLoader();
 
-    loader.load( '/3d/building.glb', function ( glb ) {
+    loader.load( '/3d/CYBER.glb', function ( glb ) {
         const model = glb.scene;
 
         scene.add( model );
@@ -37,10 +53,21 @@ const loader = new GLTFLoader();
 			if(node.isMesh){
 				node.castShadow = true;
 				node.receiveShadow = true;
+				// node.geometry = new THREE.Geometry().fromBufferGeometry( child.geometry );  
+                // node.geometry.mergeVertices(); 
+
+                // //assignUVs(child.geometry);
+
+                // node.material = material;
+                // node.verticesNeedUpdate = true;
+                // node.normalsNeedUpdate = true;
+                // node.uvsNeedUpdate = true;
+
+                // node.material.shading = THREE.SmoothShading;
+                // node.geometry.computeVertexNormals(); 
 			}
 		});
 		model.receiveShadow = true;
-
     }, undefined, function ( error ) {
 
         console.error( error );
@@ -58,35 +85,51 @@ const loader = new GLTFLoader();
 
 
 //lighting
-const directionallight1 = new THREE.DirectionalLight(0xFFFFFF, 1);
-scene.add(directionallight1);
-directionallight1.position.set(-10, 30, 0);
-directionallight1.castShadow = true;
-directionallight1.shadow.camera.bottom = -100;
+// const directionallight1 = new THREE.DirectionalLight(0xFFFFFF, 1);
+// scene.add(directionallight1);
+// directionallight1.position.set(-10, 30, 0);
+// directionallight1.castShadow = true;
+// directionallight1.shadow.camera.bottom = -100;
+// directionallight1.shadow.radius = 10;
 
-const dlight1shadowHelper = new THREE.CameraHelper(directionallight1.shadow.camera);
-scene.add(dlight1shadowHelper);
+// const dlight1shadowHelper = new THREE.CameraHelper(directionallight1.shadow.camera);
+// scene.add(dlight1shadowHelper);
+
+//spotlight
+const spotlight = new THREE.SpotLight(0xFFFFFF);
+scene.add(spotlight);
+spotlight.castShadow = true;
+// spotlight.intensity = 0.75;
+spotlight.angle = 0.2;
+
+spotlight.position.set(-100, 100, 0);
+
+const slighthelper = new THREE.SpotLightHelper(spotlight);
+scene.add(slighthelper);
 
 const ambientlight = new THREE.AmbientLight(0x333333);
 scene.add(ambientlight);
 
 
-const helper = new THREE.DirectionalLightHelper(directionallight1);
-scene.add(helper);
+// const helper = new THREE.DirectionalLightHelper(directionallight1);
+// scene.add(helper);
 
-//tes2
+// tes2
 // let step = 0;
 // let speed = 0.01;
 
 //refresh the page
 function animate() {
-	requestAnimationFrame( animate );
+	// requestAnimationFrame( animate );
 
-	//tes2
+	// tes2
 	// step += speed;
 	// ril.position.y = 10 * Math.abs(Math.sin(step)); 
 
 	renderer.render( scene, camera );
+
+	composer.render();
+	requestAnimationFrame(animate);
 };
 
 animate();
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,12 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+
 window.THREE = THREE;
 
 window.Alpine = Alpine;
@@ -15,5 +21,11 @@ window.Alpine = Alpine;
 window.GLTFLoader = GLTFLoader;
 
 window.OrbitControls = OrbitControls;
+
+window.RenderPass = RenderPass;
+
+window.EffectComposer = EffectComposer;
+
+window.UnrealBloomPass = UnrealBloomPass;
 
 Alpine.start();
